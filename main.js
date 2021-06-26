e028eedcb101e25b35db43718dc7ced804822eb2
dancroff <69163648+dancroff@users.noreply.github.com>
2021-06-26 15:32:43 -0500

    Actulizacion de portafilo

---
+++ main.js
@@ -0,0 +1,24 @@
+const header = document.querySelector("header");
+const menu = document.querySelector(".menu");
+const OpMenu = document.querySelectorAll(".opmenu");
+const btnMenu = document.querySelector(".iconMenu");
+const BtWst = document.querySelector(".BtWst");
+
+document.addEventListener("scroll", function () {
+  if (scrollY >= 10) header.classList.add("scrollActive");
+  else header.classList.remove("scrollActive");
+
+  if (scrollY >= 50) BtWst.classList.add("BtWstActive");
+  else BtWst.classList.remove("BtWstActive");
+});
+
+btnMenu.addEventListener("click", function () {
+  menu.classList.toggle("menuActive");
+});
+for(let item of OpMenu) 
+{
+  item.addEventListener("click", function () {
+    menu.classList.toggle("menuActive");
+  });
+}
+  
