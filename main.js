651cc462e55824a23a15da3c92f3423131b04534
Dancrof <bryan.caicedo@exodosoft.com>
2021-12-11 23:31:25 -0500

    ahora toca cambiar el contenido

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
+ /*====================== SELECCIONAMOS LAS CLASES O ID A UTILIZAR =========================*/
 const header = document.querySelector("header");
 const menu = document.querySelector(".menu");
 const OpMenu = document.querySelectorAll(".opmenu");
 const btnMenu = document.querySelector(".iconMenu");
-const BtWst = document.querySelector(".BtWst");
+const scrollUp = document.querySelector(".fa-arrow-up");
 
+/*====================== METODO PARA MOSTAR O OCULTAR ==================================================*/ 
 document.addEventListener("scroll", function () {
+  
+   /*====================== MUESTRA O OCULTA LA NAVEGACION =========================*/
   if (scrollY >= 10) header.classList.add("scrollActive");
   else header.classList.remove("scrollActive");
 
-  if (scrollY >= 200) BtWst.classList.add("BtWstActive");
-  else BtWst.classList.remove("BtWstActive");
+  /*====================== MUESTRA O OCULTA EL BOTON DEL SCROLL =========================*/
+  if (scrollY >= 200) scrollUp.classList.add("BtUptActive");
+  else scrollUp.classList.remove("BtUptActive");
 });
 
+/*====================== METODO PARA HACER SCROLL AN INICO DE PA PAGE ==================================================*/ 
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+})
+
+/*====================== METODO PARA OCULTAR O MOSTRAR EL MENU ==================================================*/ 
 btnMenu.addEventListener("click", function () {
   menu.classList.toggle("menuActive");
 });
+
+/*====================== METODO PARA OCULTAR EL MENU AL SELECIONAR UNA OPCION ==================================================*/ 
 for(let item of OpMenu) 
 {
   item.addEventListener("click", function () {
